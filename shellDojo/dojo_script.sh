2f517a2bacb91274cc485f9a9d1a1ad6bca32d9b
Dzso Pengo <dzsodzso63@gmail.com>
2013-04-09 20:29:25 +0200

    Dojo results 13.04.09

--- shellDojo/dojo_script.sh
+++ shellDojo/dojo_script.sh
@@ -1,6 +1,39 @@
 #!/bin/bash
 
 is_dir() 
-{ 
-    echo "Bazinga!"
+{
+    dir=$1;shift
+    msg=$1;shift
+    if [ -d "$dir" ]; then
+      echo "Directory exists"
+    else
+      if [ -n "$msg" ]; then
+        echo $msg
+      else
+        echo "The directory $dir not exist or it is not directory"
+      fi
+      return 1      
+    fi
+}
+
+is_dir_file()
+{
+  if [ -f "$1" ]; then
+    echo "File exists"
+  else
+    is_dir $1 "The file or directory $1 does not exist"
+  fi
+}
+
+lines_contains()
+{
+  [ -d "$1" -o -f "$1" ] || return 1
+  [ $# -lt 2 ] && return 1
+  
+  output=`grep -rh "$2" "$1"` 
+  if [ -z "$output" ]; then
+    echo "No result"
+  else
+    echo "$output"
+  fi
 }
--- shellDojo/run_test.sh
+++ shellDojo/run_test.sh
@@ -9,6 +9,9 @@ shuSetUp()
   #Clean up
   rm -rf tmp/
   mkdir tmp/
+  mkdir tmp/dir
+  echo -e "The first line is about a Cat and then\nin the second line we will mention the Dog\nand here is nothing." >> tmp/regular-file
+  echo -e "The first line is about a Cat and then\nin the second line we will mention the Dog\nand here is nothing.\nin the fourth line also contains Dog information" >> tmp/regular-file-with2dogs
 }
 
 TestScriptExists()
@@ -16,22 +19,116 @@ TestScriptExists()
   shuNonZeroFile dojo_script.sh
 }
 
+TestReturnsMatchesFromAllFilesIfDirectory()
+{
+  EXPECTED=$'in the second line we will mention the Dog\nin the second line we will mention the Dog\nin the fourth line also contains Dog information'
+  RESULT=`lines_contains tmp Dog`
+  test "${EXPECTED}" = "${RESULT}" 
+  shuAssert "Test lines_contains" $?
+}
+
+TestReturnLinesOfMatches()
+{
+  EXPECTED=$'in the second line we will mention the Dog\nin the fourth line also contains Dog information'
+  RESULT=`lines_contains tmp/regular-file-with2dogs Dog`
+  test "${EXPECTED}" = "${RESULT}" 
+  shuAssert "Test lines_contains" $?
+}
+
+TestReturnLineOfMatch()
+{
+  EXPECTED="in the second line we will mention the Dog"
+  RESULT=`lines_contains tmp/regular-file Dog`
+  test "${EXPECTED}" = "${RESULT}" 
+  shuAssert "Test lines_contains" $?
+}
+
+
+TestIsDirFile()
+{
+  EXPECTED="File exists" 
+  RESULT=`is_dir_file tmp/regular-file`
+  test "${EXPECTED}" = "${RESULT}" 
+  shuAssert "Test is_dir_file" $?
+  
+  EXPECTED="Directory exists" 
+  RESULT=`is_dir_file tmp/dir`
+  test "${EXPECTED}" = "${RESULT}" 
+  shuAssert "Test is_dir_file" $?
+  
+  RESULT=`is_dir_file tmp/something`
+  shuDeny "Test is_dir_file" $?
+
+  RESULT=`is_dir_file`
+  shuDeny "Test is_dir_file" $?
+
+}
+
+TestErrorMsgIfIsNotDir()
+{
+  EXPECTED="The directory tmp/something not exist or it is not directory"
+  RESULT=`is_dir tmp/something`
+  test "${EXPECTED}" = "${RESULT}" 
+  shuAssert "Test is_dir" $?
+}
+
+TestExistsWithOneIfIsNotDir()
+{
+  RESULT=`is_dir tmp/something`
+  shuDeny "Test is_dir" $?
+}
+
 TestIsDir()
 {
-    EXPECTED="hello world" 
-    RESULT=`is_dir "$EXPECTED"`
+    EXPECTED="Directory exists" 
+    RESULT=`is_dir tmp/dir`
     test "${EXPECTED}" = "${RESULT}" 
     shuAssert "Test is_dir" $?
-    EXPECTED="Bazinga!" 
-    RESULT=`is_dir "$EXPECTED"`
-    test "${EXPECTED}" = "${RESULT}" 
-    shuAssert "Test is_dir2" $?
+}
+
+TestErrorMsgIfIsNotDirOrFile()
+{
+  EXPECTED="The file or directory tmp/something3 does not exist"
+  RESULT=`is_dir_file tmp/something3`
+  test "${EXPECTED}" = "${RESULT}" 
+  shuAssert "Test is_dir_file" $?
+}
+
+TestLinesContains()
+{
+  RESULT=`lines_contains tmp/something`
+  exitcode=$?
+  test $exitcode -eq 1
+  shuAssert "Test lines_contains" $?
+  RESULT=`lines_contains tmp/regular-file`
+  exitcode=$?
+  test $exitcode -eq 1
+  shuAssert "Test lines_contains" $?
+  EXPECTED="No result"
+  RESULT=`lines_contains tmp/regular-file Elephant`
+  test "$EXPECTED" = "$RESULT"
+  shuAssert "Test lines_contains" $?
+  echo "a Dog is hidden here" > tmp/no-permission
+  chmod 000 tmp/no-permission
+  EXPECTED="No result"
+  RESULT=`lines_contains tmp/no-permission Elephant`
+  test "$EXPECTED" = "$RESULT"
+  shuAssert "Test lines_contains" $?
 }
 
 InitFunction()
 { 
     shuRegTest TestScriptExists
     shuRegTest TestIsDir 
+    shuRegTest TestExistsWithOneIfIsNotDir
+    shuRegTest TestErrorMsgIfIsNotDir
+    shuRegTest TestIsDirFile
+    shuRegTest TestErrorMsgIfIsNotDirOrFile
+    shuRegTest TestLinesContains
+    shuRegTest TestReturnLinesOfMatches
+    shuRegTest TestReturnLineOfMatch
+    shuRegTest TestReturnsMatchesFromAllFilesIfDirectory
+    #shuRegTest TestIsDir 
 } 
 
 shuStart InitFunction
